33cc51ef5926c6ea352426d8ef7cceec4bd57270
Nitish <nitishkthakur@gmail.com>
2025-05-02 09:09:48 +0530

    From Phuntsholing

--- main.py
+++ main.py
@@ -82,5 +82,5 @@ async def search(request: Request, query: str = Form(...)):
     weather_data_['request'] = request
     
     # Render the HTML template with the response data
-    r#eturn templates.TemplateResponse("index.html", weather_data_)
+    #return templates.TemplateResponse("index.html", weather_data_)
     return templates.TemplateResponse("templates/llm_response.html", {"request": request, "response": response})
